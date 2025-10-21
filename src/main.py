5ebc5edec6b742e62f797c80d226a53e37f3c1e1
Julius Foverskov <julius.foverskov1@gmail.com>
2025-10-21 17:30:24 +0200

    reorder imports to fit parent-child relations in quick&dirty main script

--- src/main.py
+++ src/main.py
@@ -1,22 +1,66 @@
+import numpy as np
+import pandas as pd
+
 import config
 import utils
 from connection import DatabaseConnection
 from table import Table
-import pandas as pd
 
 
 def main():
     # staff_translation = {"name": "staff_first_name", "last_name": "staff_first_name"}
     # data = config.RAW_DATA_DIR / "csv" / "staffs.csv"
 
-    with DatabaseConnection(config.dbconfig) as connection:
-        print(connection.is_connected())
-        order_items = Table("customers", connection)
+    with DatabaseConnection(config=config.dbconfig) as connection:
+        order_items = Table(table_name="order_items", connection=connection)
+        brands = Table(table_name="brands", connection=connection)
+        categories = Table(table_name="categories", connection=connection)
+        products = Table(table_name="products", connection=connection)
+        stocks = Table(table_name="stocks", connection=connection)
+        orders = Table(table_name="orders", connection=connection)
+        customers = Table(table_name="customers", connection=connection)
+        staffs = Table(table_name="staffs", connection=connection)
+        stores = Table(table_name="stores", connection=connection)
 
-        order_items_data = pd.read_csv(config.RAW_DATA_DIR / "api" / "data" / "order_items.csv").to_dict("records")
-        
-        utils.run_sql_schema(config.DB_SCHEMA, connection=connection)
-        order_items.insertmany(order_items_data)
+        order_items_data = pd.read_csv(config.ORDER_ITEMS_FILE).to_dict("records")
+        brands_data = pd.read_csv(config.BRANDS_FILE).to_dict("records")
+        categories_data = pd.read_csv(config.CATEGORIES_FILE).to_dict("records")
+        products_data = pd.read_csv(config.PRODUCTS_FILE).to_dict("records")
+
+        stocks_data = pd.read_csv(config.STOCKS_FILE).to_dict("records")
+        orders_data = (
+            pd.read_csv(config.ORDERS_FILE).convert_dtypes().to_dict("records")
+        )
+
+        ##orders_data = orders_data.to_dict("records")
+        customers_data = (
+            pd.read_csv(config.CUSTOMERS_FILE)
+            .replace({np.nan: None})
+            .to_dict("records")
+        )
+        staffs_data = (
+            pd.read_csv(config.STAFFS_FILE).replace({np.nan: None}).to_dict("records")
+        )
+        stores_data = (
+            pd.read_csv(config.STORES_FILE).replace({np.nan: None}).to_dict("records")
+        )
+
+        utils.run_sql_schema(file=config.DB_SCHEMA, connection=connection)
+
+        # No children
+        brands.insertmany(data=brands_data)
+        categories.insertmany(data=categories_data)
+        stores.insertmany(data=stores_data)
+        customers.insertmany(data=customers_data)
+        staffs.insertmany(data=staffs_data)
+
+        products.insertmany(data=products_data)
+
+        stocks.insertmany(data=stocks_data)
+
+        orders.insertmany(data=orders_data)
+        order_items.insertmany(data=order_items_data)
+
 
 if __name__ == "__main__":
     main()
