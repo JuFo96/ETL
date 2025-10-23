4341b7225cd63fc68357a29622125102a60f8b49
Julius Foverskov <julius.foverskov1@gmail.com>
2025-10-23 17:21:08 +0200

    add topological sort based on kahns algorithm, to get insertion order to dbs

--- src/utils.py
+++ src/utils.py
@@ -1,57 +1,68 @@
 from pathlib import Path
 
 import pandas as pd
-import mysql.connector
-import config
-from dataclasses import asdict
+from collections import OrderedDict, deque
 
 
 def transform_column_names(file: Path, name_translation: dict):
-    
     data = pd.read_csv(file)
-    #print(data.columns)
+    # print(data.columns)
     renamed_df = data.rename(columns=name_translation)
-    
+
     return renamed_df
 
-def print_column_names():
-    pass
 
+def topological_sort(graph: dict[str, set[str]]) -> list[str]:
+    """Topological sort based on Kahn's Algorithm.
 
+    Args:
+        Dictionary containing all tables with values given by their dependencies.
 
+    Returns:
+        List of tables names sorted in topological order.
+    """
+    results = []
 
+    # Sorts dict by amount of parent tables, low to high
+    sorted_graph = OrderedDict(sorted(graph.items(), key=lambda x: len(x[1])))
 
-def run_sql_schema(file: Path, connection) -> None:
-    with open(file, "r") as f:
-        sql = f.read()
-    with connection.cursor() as cur:
-        for statement in sql.split(";"):
-            statement = statement.strip()
-            if statement:
-                cur.execute(statement)
-        connection.commit()
+    queue = deque([node for node, edge in sorted_graph.items() if not edge])
 
+    while queue:
+        free_node = queue.popleft()
+        results.append(free_node)
 
+        # Remove nodes with no dependencies from all other nodes dependencies
+        for node in sorted_graph:
+            if free_node in sorted_graph[node]:
+                sorted_graph[node].discard(free_node)
 
+                # If node now has no dependencies, add it to the queue
+                if not sorted_graph[node]:
+                    queue.append(node)
 
+    if len(results) != len(sorted_graph):
+        remaining = [node for node in sorted_graph if node not in results]
+        raise ValueError(f"Cyclic dependency detected between nodes: {remaining}")
 
+    return results
 
 
 def main():
-    #staff_translation = {"name": "staff_first_name", "last_name": "staff_first_name"}
-    #data = config.RAW_DATA_DIR / "csv" / "staffs.csv"
-    
-    db_cred = asdict(config.dbconfig) 
-    connection = mysql.connector.connect(**db_cred)
-    print(connection.is_connected())
-    run_sql_schema(config.DB_SCHEMA, connection=connection)
-    
 
-    #print(df.columns)
+    graph = {
+        "customers": {"orders"},
+        "order_items": {"orders", "products"},
+        "orders": {"customers", "stores"},
+        "staffs": set(),
+        "stores": set(),
+        "brands": set(),
+        "categories": set(),
+        "products": {"brands", "categories"},
+        "stocks": {"products", "stores"},
+    }
 
-
-
-
+    print(topological_sort(graph))
 
 
 if __name__ == "__main__":
