330a9d98707b08b7b4583984591b7fa996db36b0
Julius Foverskov <julius.foverskov1@gmail.com>
2025-11-06 20:12:14 +0100

    refactor remove redudant code, make main idempotent with, but is currently truncating entire db on run

--- src/main.py
+++ src/main.py
@@ -1,29 +1,22 @@
-from numpy import insert
-import pandas as pd
+import time
 
+import pandas as pd
+from psycopg import sql
 import config
 import utils
 from connection import DatabaseConnection
+from extract import extract_all_sources
 from schema import Schema
 from table import Table
-from extract import extract_all_sources
 from transform import transform_all_tables
-import time
 
-
-def setup_db(database: Schema, connection: DatabaseConnection) -> list[str]:
-    #database.run_sql_schema(connection)
-    insert_order = utils.get_insert_order(
-            sql_procedure_path=config.DEPENDENCIES_PROCEDURE, connection=connection
-        )
-    if not insert_order:
-        raise ValueError("Table insert order is empty: check get_depencies.sql")
-    return insert_order
 
 def load_db(database: Schema, connection: DatabaseConnection) -> None:
     """Does something"""
-    insert_order = setup_db(database, connection)
-    
+    insert_order = utils.get_insert_order(
+        sql_procedure_path=config.DEPENDENCIES_PROCEDURE, connection=connection
+    )
+
     tables: dict[str, Table] = {}
     for table in insert_order:
         columns = set(database.get_columns(table))
@@ -41,12 +34,12 @@ def main():
     extract_all_sources()
     transform_all_tables(config.RAW_FILES)
 
-    integrated_db = Schema(schema_file_path=config.DB_SCHEMA)
+    bikestore_db = Schema(schema_file_path=config.DB_SCHEMA)
     with DatabaseConnection(config=config.dbconfig) as connection:
         with connection.cursor() as cur:
+            cur.execute("""CALL bikestore.truncate_data_tables();""")
             cur.execute("set search_path to bikestore;")
-        load_db(integrated_db, connection)
-
+        load_db(bikestore_db, connection)
 
 
 if __name__ == "__main__":
